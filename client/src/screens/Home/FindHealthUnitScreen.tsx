b34d911a92b8dd8c3e9771e3b7ae5bbca631e2fd
TinnaphoomK <88415686+TinnaphoomK@users.noreply.github.com>
2023-10-11 01:29:00 +0700

    Update FindHealthUnitScreen.tsx

--- client/src/screens/Home/FindHealthUnitScreen.tsx
+++ client/src/screens/Home/FindHealthUnitScreen.tsx
@@ -14,6 +14,15 @@ const FindHealthUnitScreen: React.FC<FindHealthUnitScreenProps> = (props) => {
     longitude: 0, // Updated with the current longitude
   });
 
+  const [initialRegion, setInitialRegion] = useState({
+    latitude: 0,
+    longitude: 0,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  const [useCurrentLocation, setUseCurrentLocation] = useState(true); // Track whether to use current location
+
   useEffect(() => {
     // When the component mounts, get the current location and set the map's initial region
     (async () => {
@@ -23,11 +32,25 @@ const FindHealthUnitScreen: React.FC<FindHealthUnitScreenProps> = (props) => {
         return;
       }
 
-      const location = await Location.getCurrentPositionAsync({});
-      const { latitude, longitude } = location.coords;
-      setMarkerCoordinates({ latitude, longitude });
+      try {
+        const location = await Location.getCurrentPositionAsync({});
+        const { latitude, longitude } = location.coords;
+        setMarkerCoordinates({ latitude, longitude });
+
+        // Set the initial region based on the state
+        if (useCurrentLocation) {
+          setInitialRegion({
+            latitude,
+            longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          });
+        }
+      } catch (error) {
+        console.error("Error fetching current location:", error);
+      }
     })();
-  }, []);
+  }, [useCurrentLocation]);
 
   const handleSearch = async () => {
     try {
@@ -39,6 +62,15 @@ const FindHealthUnitScreen: React.FC<FindHealthUnitScreenProps> = (props) => {
           latitude,
           longitude,
         });
+        // Always set the initial region to the marker's location when searching
+        setInitialRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        });
+        // Update the state to stop using current location as the initial region
+        setUseCurrentLocation(false);
       }
     } catch (error) {
       console.error("Error fetching location:", error);
@@ -59,20 +91,8 @@ const FindHealthUnitScreen: React.FC<FindHealthUnitScreenProps> = (props) => {
       </View>
 
       {/* Map */}
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: markerCoordinates.latitude,
-          longitude: markerCoordinates.longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      >
-        <Marker
-          coordinate={markerCoordinates}
-          title="Health Unit Location"
-          description="Health unit description"
-        />
+      <MapView style={styles.map} initialRegion={initialRegion}>
+        <Marker coordinate={markerCoordinates} title="Health Unit Location" />
       </MapView>
     </View>
   );
