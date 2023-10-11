ec19f68d751460109796677b2e8a6eac980c9639
7E8J <103146285+7E8J@users.noreply.github.com>
2023-10-11 10:56:44 +0700

    [Update] news-create-dto
    add image attri

--- server/src/news/dto/news-create.dto.ts
+++ server/src/news/dto/news-create.dto.ts
@@ -5,6 +5,9 @@ export class NewsCreateDto {
   @ApiProperty({ type: 'string', description: 'Title', example: 'News Title' })
   title: string;
 
+  @ApiProperty({ type: 'string', format: 'binary', description: 'Image file'})
+  image?: any;
+
   @ApiProperty({ type: 'string', description: 'Description', example: 'News Description' })
   description: string;
 }
