7a0526d9361279bec94804cf5da48098520a92df
TinnaphoomK <88415686+TinnaphoomK@users.noreply.github.com>
2023-10-25 04:47:49 +0700

    Update EditAddressScreen.tsx

--- client/src/screens/Settings/EditAddressScreen.tsx
+++ client/src/screens/Settings/EditAddressScreen.tsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 import { Button } from "../../core/components/Button";
 import { styles } from "./style/EditAddressScreen.style";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage"; 
+import { set } from "date-fns";
+
+
 
 const ProfileEditScreen = ({ navigation }: any) => {
+  const [name, setName] = useState(""); // State to store the name
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const userId = await AsyncStorage.getItem("userId");
+        const response = await axios.get(
+          `http://localhost:3000/address/${userId}`
+        );
+        const address = response.data.address.address;
+        setName(address);
+        console.log(address);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+    fetchUserData();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.border}>
         <Text style={styles.title}>Current location</Text>
-        <Text style={styles.address}>123 Main Street, City, Country asndadadas, Thalind, 10230</Text>
+        <Text style={styles.address}>{name}</Text>
       </View>
       <Button
           title="Change Location"
