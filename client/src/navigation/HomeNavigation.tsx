c073b5a33d3551df18d5654767b473939d89dbb6
7E8J <103146285+7E8J@users.noreply.github.com>
2023-10-24 19:45:54 +0700

    [fix] navigation capital letter

--- client/src/navigation/HomeNavigation.tsx
+++ client/src/navigation/HomeNavigation.tsx
@@ -1,9 +1,9 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Home from "../screens/Home/HomeScreen";
-import DonateRequestScreen from "../screens/Home/DonateRequestScreen/DonateRequestScreen"
-import MyAppointmentScreen from "../screens/Home/MyAppointmentScreen/MyAppointmentScreen";
-import FindHealthUnitScreen from "../screens/Home/FindHealthUnitScreen";
-import PostDetailScreen from "../screens/Post/PostDetailScreen";
+import Home from "../screens/home/HomeScreen";
+import DonateRequestScreen from "../screens/home/DonateRequestScreen/DonateRequestScreen"
+import MyAppointmentScreen from "../screens/home/MyAppointmentScreen/MyAppointmentScreen";
+import FindHealthUnitScreen from "../screens/home/FindHealthUnitScreen";
+import PostDetailScreen from "../screens/post/PostDetailScreen";
 
 const Tab = createNativeStackNavigator();
 
