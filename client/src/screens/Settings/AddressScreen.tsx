7eafdf56e694e53ae832d19d4a6524b329b118d8
TinnaphoomK <88415686+TinnaphoomK@users.noreply.github.com>
2023-10-25 04:10:33 +0700

    Update AddressScreen.tsx

--- client/src/screens/Settings/AddressScreen.tsx
+++ client/src/screens/Settings/AddressScreen.tsx
@@ -12,6 +12,9 @@ import * as Location from "expo-location";
 import { FontAwesome } from "@expo/vector-icons";
 import { styles } from "./style/AddressScreen.style";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 
 interface LocationScreenProps {
   // Define any props your component needs here
@@ -26,9 +29,7 @@ const LocationScreen: React.FC<LocationScreenProps> = () => {
   });
   const [loadingLocation, setLoadingLocation] = useState(true);
   const [name, setName] = useState(""); // State to store the name
-  const [savedLocations, setSavedLocations] = useState<
-    { name: string; latitude: number; longitude: number }[]
-  >([]);
+  const [latestMarker, setLatestMarker] = useState<{ latitude: number; longitude: number } | null>(null);
 
   useEffect(() => {
     const getLocation = async () => {
@@ -57,31 +58,23 @@ const LocationScreen: React.FC<LocationScreenProps> = () => {
     getLocation();
   }, []);
 
-  const handleSave = () => {
-    if (name && region.latitude !== 0 && region.longitude !== 0) {
-      // Create a new saved location object
-      const newLocation = {
-        name,
-        latitude: region.latitude,
-        longitude: region.longitude,
-      };
-
-      // Update the list of saved locations
-      setSavedLocations([...savedLocations, newLocation]);
-
-      // Clear the name and region
-      setName("");
-      setRegion({
-        latitude: 0,
-        longitude: 0,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01,
-      });
-    }
+  const handleSave = async() => {
+    try {
+      const { data: res } = await axios.post(
+      "http://localhost:3000/user/update",
+      {
+        id: await AsyncStorage.getItem("userId"),
+        name: name,
+        latitude: latestMarker?.latitude,
+        longitude: latestMarker?.longitude,
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
   };
 
   const handleSelectLocation = (data: any, details: any) => {
-    // Extract the latitude and longitude from the selected location
     const { lat, lng } = details.geometry.location;
     setRegion({
       latitude: lat,
@@ -89,11 +82,58 @@ const LocationScreen: React.FC<LocationScreenProps> = () => {
       latitudeDelta: 0.01,
       longitudeDelta: 0.01,
     });
+
+    // Update the latest marker when a new location is selected
+    setLatestMarker({ latitude: lat, longitude: lng });
+    setName(data.description);
+  };
+  console.log(name);
+  console.log(latestMarker);
+  
+  const handleMapPress = async (event: any) => {
+    // When the user taps on the map, you can place a marker at the tapped location
+    const { latitude, longitude } = event.nativeEvent.coordinate;
+    setLatestMarker({ latitude, longitude });
+
+    // Reverse geocode the coordinates to get the name
+    const locationName = await reverseGeocode(latitude, longitude);
+
+    // Set the name in the search bar
+    setName(locationName);
+  };
+
+
+  const reverseGeocode = async (latitude: number, longitude: number) => {
+    try {
+      const response = await axios.get(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyDzrf9J0CJvAGlHEHaBXTggIGG0hfF96ug`
+      );
+
+      if (response.data.results.length > 0) {
+        const locationName = response.data.results[0].formatted_address;
+        return locationName;
+      } else {
+        console.error("No results found for reverse geocoding.");
+        return "Custom Location"; // Default name if no results found
+      }
+    } catch (error) {
+      console.error("Error during reverse geocoding:", error);
+      return "Custom Location"; // Default name on error
+    }
   };
 
   return (
     <View style={styles.container}>
-      <View style={{ backgroundColor: "#E99999", height: 270 }}>
+      <View
+        style={{
+          backgroundColor: "#E99999",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          zIndex: 1,
+        }}
+      >
         <GooglePlacesAutocomplete
           placeholder="Search Location"
           onPress={handleSelectLocation}
@@ -102,11 +142,11 @@ const LocationScreen: React.FC<LocationScreenProps> = () => {
             key: "AIzaSyDzrf9J0CJvAGlHEHaBXTggIGG0hfF96ug",
             language: "en",
           }}
-          listViewDisplayed="auto" // Set this to "auto" to display suggestions without worrying about height
+          styles={{ position: "absolute" }}
         />
       </View>
 
-      <View style={styles.panel}>
+      <View style={[styles.panel, { marginTop: 50 }]}>
         <View style={styles.panelContent}>
           <FontAwesome
             name="map-marker"
@@ -125,14 +165,21 @@ const LocationScreen: React.FC<LocationScreenProps> = () => {
           <Text>Loading your location...</Text>
         </View>
       ) : (
-        <MapView style={styles.map} region={region} provider={PROVIDER_GOOGLE}>
-          <Marker
-            coordinate={{
-              latitude: region.latitude,
-              longitude: region.longitude,
-            }}
-            title="Your location"
-          />
+        <MapView
+          style={styles.map}
+          region={region}
+          provider={PROVIDER_GOOGLE}
+          onPress={handleMapPress}
+        >
+          {latestMarker && (
+            <Marker
+              coordinate={{
+                latitude: latestMarker.latitude,
+                longitude: latestMarker.longitude,
+              }}
+              title="Selected Location"
+            />
+          )}
         </MapView>
       )}
 
@@ -144,5 +191,6 @@ const LocationScreen: React.FC<LocationScreenProps> = () => {
     </View>
   );
 };
+
 
 export default LocationScreen;
